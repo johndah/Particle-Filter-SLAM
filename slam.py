393374d1e5bbea4d2319097d4f963dea57c7189b
John Dahlberg <john.dahlberg@live.com>
2018-12-17 15:07:57 +0100

    Update slam.py
    Created landmark class

--- slam.py
+++ slam.py
@@ -1,7 +1,31 @@
+from numpy import *
+import copy
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+class Landmark(object):
+
+    def __init__(self, x, y, index):
+        self.x = x
+        self.y = y
+        self.index = index
+
+def plotMap(landmarks):
+    plt.figure()
+    plt.plot(landmarks[0].x, landmarks[0].y, 'go')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Map')
 
 
 def main():
-    print("Doing more slam things")
+
+    landmarks = []
+    landmarks.append(Landmark(1, 2, 1))
+
+    plotMap(landmarks)
 
 if __name__ == '__main__':
     main()
+    plt.show()
