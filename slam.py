82d04be7c5e4aa18e052e9b3bf9309eccc510daa
John Dahlberg <john.dahlberg@live.com>
2018-12-18 10:28:14 +0100

    Noisy observation

--- slam.py
+++ slam.py
@@ -19,22 +19,30 @@ def getMeasurements(robot_pose):
     for landmark in landmarks:
         dx = landmark.x - robot_pose[0]
         dy = landmark.y - robot_pose[1]
-        r = sqrt(dx**2 + dy**2) + rand
+        r = sqrt(dx**2 + dy**2) + 0.1*random.randn()
         if r < 5:
-            alpha = arctan2(dy, dx)
+            alpha = arctan2(dy, dx) + 0.1*random.randn()
             measurements.append([r, alpha])
 
     return measurements
 
-def plotMap():
+def plotMap(robot_pose, measurements):
 
+    plt.cla()
     for landmark in landmarks:
         plt.plot(landmark.x, landmark.y, 'go')
         plt.text(landmark.x, landmark.y + .05, '(' + str(landmark.index) + ')')
-    plt.axis([0, 25, 5, 20])
+
+    for measure in measurements:
+        r = measure[0]
+        alpha = measure[1]
+        plt.plot([robot_pose[0], robot_pose[0] + r*cos(alpha)], [robot_pose[1], robot_pose[1] + r*sin(alpha)], 'b')
+
+    plt.axis([0, 25, 0, 25])
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Map')
+    plt.pause(1e-3)
 
 def initMap():
 
@@ -48,11 +56,11 @@ def initMap():
 
 def particleFilterSlam():
 
-    robot_pose = [7, 10, 0]
-    measurements = getMeasurements(robot_pose)
-    print(measurements)
+    for i in range(1 ,100):
+        robot_pose = [7, 12, 0]
+        measurements = getMeasurements(robot_pose)
 
-    plotMap()
+        plotMap(robot_pose, measurements)
 
 def main():
 
@@ -60,5 +68,6 @@ def main():
     particleFilterSlam()
 
 if __name__ == '__main__':
+    random.seed(0)
     main()
     plt.show()
