2e123e9f8a1857dcddf70abf7632b71dcca433f4
bonnevie <bonnevie@kth.se>
2018-12-19 15:54:00 +0100

    Motion models and particle init

--- motion.py
+++ motion.py
@@ -1,13 +1,18 @@
 from numpy import *
 
 
-def motion_model_prediction(v, omega, mu, delta_t):
-    # Calculates prediction u given v, omega, mu, and delta_t; u = (dx,dy,dtheta)
-    dx = v*delta_t*cos(mu[2])
-    dy = v*delta_t*sin(mu[2])-1e-3  # simulating error in motion model
-    dtheta = omega*delta_t+1e-2  # simulating error in motion model
-    u = [dx, dy, dtheta]
-    return u
+def motion_model_prediction(v, omega, S, R, delta_t):
+    # Calculates prediction on the particle set S given v, omega, S, R and delta_t
+    M = size(S[1,:]) # Number of particles
+    dx = array([v*delta_t*cos(S[2,:])])
+    dy = array([v*delta_t*sin(S[2,:])-1e-3])  # simulating error in motion model
+    dtheta = ones([1, M])*(omega*delta_t+1e-2)  # simulating error in motion
+    u = concatenate((dx, dy, dtheta))
+    noise = dot(R,random.randn(3, M))  # Element wise multiplication
+    print(u.shape)
+    print(noise.shape)
+    S[0:3,:] = S[:3,:] + u + noise
+    return S
 
 
 def motion_model_gt(v, omega, mu, delta_t):
@@ -18,6 +23,10 @@ def motion_model_gt(v, omega, mu, delta_t):
     u = [dx, dy, dtheta]
     return u
 
-
-
+'''
+def motion_path():
+    start = 0
+    stop = 2
+    for t in linspace(start,stop,(stop-start)/delta_t):
+'''
 
--- particle_filter.py
+++ particle_filter.py
@@ -1,4 +1,6 @@
 from numpy import *
+import matplotlib.pyplot as plt
+from motion import *
 
 
 def particle_init(window, M, start_pose = []):
@@ -7,13 +9,35 @@ def particle_init(window, M, start_pose = []):
     sigma_xy = 1e-1  # Variance in starting position for known pose for x and y
     sigma_theta = 1e-1  # Variance in starting position for known pose for theta
     S = zeros([4, M])
-    if not start_posea:  # If start_pose is empty
+    if not start_pose:  # If start_pose is empty
         S[0, :] = random.uniform(window[0], window[1], [1, M])
-        S[1, :] = random.uniform(window[0], window[1], [1, M])
-        S[1, :] = random.uniform(-pi/2, pi/2, [1, M])
+        S[1, :] = random.uniform(window[2], window[3], [1, M])
+        S[2, :] = random.uniform(-pi/2, pi/2, [1, M])
     else:
         S = zeros([4, M])  # If start_pose is given the particle set will be gaussians around the starting position
         S[0, :] = start_pose[0]+random.randn(1, M)*sigma_xy
         S[1, :] = start_pose[1]+random.randn(1, M)*sigma_xy
-        S[1, :] = start_pose[2]+random.randn(-pi / 2, pi / 2, [1, M])*sigma_theta
+        S[1, :] = start_pose[2]+random.randn(1, M)*sigma_theta
     return S
+
+
+def plot_particle_set(S, figure):
+    #  Plots particle set S in figure figure
+    #  S has dimensions 4xM where M in the number of particles
+    plt.scatter(S[0,:],S[1,:])
+
+
+def main():
+    window = [0,5,0,5]
+    S = particle_init(window, 100)
+    fig1 = plt.figure()
+    plot_particle_set(S, fig1)
+
+    R = diag([0,0,0])
+    delta_t = 1
+    S = motion_model_prediction(1, 0, S, R, delta_t)
+    fig2 = plt.figure()
+    plot_particle_set(S, fig2)
+    plt.show(fig1)
+main()
+
