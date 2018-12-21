91e56d4f101c4031b1e715808f1b19ef1d52b4b4
bonnevie <bonnevie@kth.se>
2018-12-21 18:55:01 +0100

    stuff

--- particle_filter.py
+++ particle_filter.py
@@ -9,7 +9,7 @@ def particle_init(window, M, start_pose = []):
     sigma_xy = 1e-1  # Variance in starting position for known pose for x and y
     sigma_theta = 1e-1  # Variance in starting position for known pose for theta
     S = zeros([4, M])
-    if not start_pose:  # If start_pose is empty
+    if not start_pose:  # If start_pose is empty, wont be used
         S[0, :] = random.uniform(window[0], window[1], [1, M])
         S[1, :] = random.uniform(window[2], window[3], [1, M])
         S[2, :] = random.uniform(-pi/2, pi/2, [1, M])
@@ -27,7 +27,7 @@ def plot_particle_set(S, figure):
     plt.scatter(S[0,:],S[1,:])
 
 
-def systematic_resample(S):
+def systematic_resample(S):  # Must include map resample
     M = S.shape[1]
     cdf = cumsum(S[3,:])
 
@@ -44,6 +44,30 @@ def systematic_resample(S):
 
     return S_new
 
+
+def weight(S, Psi, outlier):
+    # Adds weights to the last row in S
+    # Psi in on the [n, M] where n is the number of measurements an M is the number of particles
+    # outlier contains information about measurement outliers
+    pz = prod(Psi[where(1-outlier)], 1)  # Non normalized weights without outliers
+    w = pz*1/sum(pz)  # Normalization
+    S[4,:] = w
+    return S
+
+def measurement_model(W, S):
+    # W is the location of the landmarks on each particles map. Shape [2*landmarks, particles]
+    # S is the particle set. Shape [4, particles]
+    # h is predicted measurements. Shape [2*landmarks, particles]
+    no_landmarks = W.shape[0]/2
+    xindices = arange(0,no_landmarks+1,2)
+    yindices = xindices + 1
+
+    h = [sqrt(square(W[xindices] - S[0,:]) + square(W[yindices] - S[1,:]) ), # inputa varannan
+    atan2(W(yindices) - S(2,:), W(xindices) - S(1,:)) - S(3,:)];
+
+    h(2,:) = mod(h(2,:)+pi, 2 * pi)-pi;
+
+    return h
 '''
 def main():
     window = [0,5,0,5]
